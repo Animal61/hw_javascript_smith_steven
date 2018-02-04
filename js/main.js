33a2884ef73b286addfc6e506cbdb0fced28d045
Kelley Smith <s.kelley.smith@hotmail.com>
2018-02-04 11:51:28 -0500

    Adding iceCream variable to main.js

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// for arrays branch
+var iceCream = ['vanilla', 'chocolate chip cookie dough', 'cookies and cream'];
+
+// add Animal's Addiction to iceCream variable
+iceCream.push('Animal\'s Addiction');
